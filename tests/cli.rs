f4e2ad514a518e820ad72b13be2e85daad2963fc
秦宇航 <qinyuhangxiaoxiang@gmail.com>
2022-03-20 15:24:48 +0800

    ch1

---
+++ tests/cli.rs
@@ -0,0 +1,32 @@
+use std::process::Command;
+use assert_cmd::{Command as A_CMD};
+
+#[test]
+fn test_canary() {
+    assert!(true);
+}
+
+#[test]
+fn test_ls() {
+    let mut c = Command::new("ls");
+    assert!(c.output().is_ok());
+}
+
+
+#[test]
+fn test_true() {
+    let mut c = Command::new("./target/debug/true");
+    assert!(c.output().is_ok());
+}
+
+#[test]
+fn test_false() {
+    let mut c = A_CMD::cargo_bin("false").unwrap();
+    c.assert().failure();
+}
+
+#[test]
+fn test_hello() {
+    let mut c = A_CMD::cargo_bin("hello").unwrap();
+    c.assert().success().stdout("Hello world!\n");
+}
